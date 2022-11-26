5b1977aad2ae96b67e12456e431389a049c5b291
Vasjan <vasjancupri04@gmail.com>
2022-11-26 14:22:42 +0100

    handle form submission

---
+++ scripts/config.js
@@ -0,0 +1,15 @@
+function openPlayerConfig() {
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get("playername");
+}
